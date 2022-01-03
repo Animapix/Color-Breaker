d4e85ed070e0b4364c4876adc42815e7a23477cf
Nicolas POINTET <inovasong@me.com>
2022-01-03 14:33:11 +0100

    Add Service locator

---
+++ Services/Services.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animapix
+{
+    public static class Services
+    {
+        private static readonly Dictionary<Type, object> _listeServices = new Dictionary<Type, object>();
+
+        public static void RegisterService<T>(T service)
+        {
+            _listeServices[typeof(T)] = service;
+        }
+
+        public static T Get<T>()
+        {
+            return (T)_listeServices[typeof(T)];
+        }
+    }
+}
